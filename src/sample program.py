7e7423c45a57f9f03ee8872e87419014c80aa67f
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-10-03 10:38:53 +0800

    Create sample program.py

---
+++ src/sample program.py
@@ -0,0 +1,22 @@
+    if marker == 'O':
+        if sum(total_list) < a:
+            if int(sum(total_list) + int(min((df.loc[i,'Target'],new_list[i-1])))) == a:
+                df.loc[i,str(day_2 + 5)] = int(min((df.loc[i,'Target'],new_list[i-1])))
+                total_list.append(df.loc[i,str(day_2 + 5)])
+            elif int(sum(total_list) + int(min((df.loc[i,'Target'],new_list[i-1])))) > a:
+                if df.loc[i,str(day_2 + 5)] == int(min((df.loc[i,'Target'],new_list[i-1]))):
+                    pass
+                else:
+                    df.loc[i,str(day_2 + 5)] = a - sum(total_list)
+                    total_list.append(df.loc[i,str(day_2 + 5)])
+    if marker_2 == 'DO':
+        if sum(total_list) < a:
+            if int(sum(total_list) + int(min((df.loc[i,'Target'],new_list[i-1])))) == a:
+                df.loc[i,str(day_2 + 6)] = int(min((df.loc[i,'Target'],new_list[i-1])))
+                total_list.append(df.loc[i,str(day_2 + 6)])
+            elif int(sum(total_list) + int(min((df.loc[i,'Target'],new_list[i-1])))) > a:
+                if df.loc[i,str(day_2 + 6)] == int(min((df.loc[i,'Target'],new_list[i-1]))):
+                    pass
+                else:
+                    df.loc[i,str(day_2 + 6)] = a - sum(total_list)
+                    total_list.append(df.loc[i,str(day_2 + 6)])
