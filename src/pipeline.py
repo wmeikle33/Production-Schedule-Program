5b815fac9b5672a60fa8f272bae91a44d66748ce
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-01 11:12:46 +0800

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,27 +1,27 @@
-def pipeline(day, total_list, market, max_sum, new_list):
+def pipeline(day, value_list, marker, max_sum, new_values):
     if marker == 'O':
-        if sum(total_list) < a:
-            if int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) == a:
-                df.loc[i,str(day + 5)] = int(min((df.loc[i,'Column'],new_list[i-1])))
-                total_list.append(df.loc[i,str(day + 5)])
-            elif int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) > a:
-                if df.loc[i,str(day + 5)] == int(min((df.loc[i,'Column'],new_list[i-1]))):
+        if sum(value_list) < a:
+            if int(sum(value_list) + int(min((df.loc[i,'Column'],new_values[i-1])))) == a:
+                df.loc[i,str(day + 5)] = int(min((df.loc[i,'Column'],new_values[i-1])))
+                value_list.append(df.loc[i,str(day + 5)])
+            elif int(sum(value_list) + int(min((df.loc[i,'Column'],new_values[i-1])))) > a:
+                if df.loc[i,str(day + 5)] == int(min((df.loc[i,'Column'],new_values[i-1]))):
                     pass
                 else:
-                    df.loc[i,str(day + 5)] = a - sum(total_list)
-                    total_list.append(df.loc[i,str(day + 5)])
+                    df.loc[i,str(day + 5)] = a - sum(value_list)
+                    value_list.append(df.loc[i,str(day + 5)])
     if marker_2 == 'DO':
-        if sum(total_list) < a:
-            if int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) == a:
-                df.loc[i,str(day + 6)] = int(min((df.loc[i,'Column'],new_list[i-1])))
-                total_list.append(df.loc[i,str(day + 6)])
-            elif int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) > a:
-                if df.loc[i,str(day + 6)] == int(min((df.loc[i,'Column'],new_list[i-1]))):
+        if sum(value_list) < a:
+            if int(sum(value_list) + int(min((df.loc[i,'Column'],new_values[i-1])))) == a:
+                df.loc[i,str(day + 6)] = int(min((df.loc[i,'Column'],new_values[i-1])))
+                value_list.append(df.loc[i,str(day + 6)])
+            elif int(sum(value_list) + int(min((df.loc[i,'Column'],new_values[i-1])))) > a:
+                if df.loc[i,str(day + 6)] == int(min((df.loc[i,'Column'],new_values[i-1]))):
                     pass
                 else:
-                    df.loc[i,str(day + 6)] = a - sum(total_list)
-                    total_list.append(df.loc[i,str(day + 6)])
-    return total_list
+                    df.loc[i,str(day + 6)] = a - sum(value_list)
+                    value_list.append(df.loc[i,str(day + 6)])
+    return value_list
 
 def main()
     pipeline()
