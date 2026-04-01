3a98d1a7ff3b369df7b3e51559b1f41582f9637a
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-01 11:08:56 +0800

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,26 +1,26 @@
-def pipeline(day_2, total_list, market, a, new_list):
+def pipeline(day, total_list, market, max_sum, new_list):
     if marker == 'O':
         if sum(total_list) < a:
             if int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) == a:
-                df.loc[i,str(day_2 + 5)] = int(min((df.loc[i,'Column'],new_list[i-1])))
-                total_list.append(df.loc[i,str(day_2 + 5)])
+                df.loc[i,str(day + 5)] = int(min((df.loc[i,'Column'],new_list[i-1])))
+                total_list.append(df.loc[i,str(day + 5)])
             elif int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) > a:
-                if df.loc[i,str(day_2 + 5)] == int(min((df.loc[i,'Column'],new_list[i-1]))):
+                if df.loc[i,str(day + 5)] == int(min((df.loc[i,'Column'],new_list[i-1]))):
                     pass
                 else:
-                    df.loc[i,str(day_2 + 5)] = a - sum(total_list)
-                    total_list.append(df.loc[i,str(day_2 + 5)])
+                    df.loc[i,str(day + 5)] = a - sum(total_list)
+                    total_list.append(df.loc[i,str(day + 5)])
     if marker_2 == 'DO':
         if sum(total_list) < a:
             if int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) == a:
-                df.loc[i,str(day_2 + 6)] = int(min((df.loc[i,'Column'],new_list[i-1])))
-                total_list.append(df.loc[i,str(day_2 + 6)])
+                df.loc[i,str(day + 6)] = int(min((df.loc[i,'Column'],new_list[i-1])))
+                total_list.append(df.loc[i,str(day + 6)])
             elif int(sum(total_list) + int(min((df.loc[i,'Column'],new_list[i-1])))) > a:
-                if df.loc[i,str(day_2 + 6)] == int(min((df.loc[i,'Column'],new_list[i-1]))):
+                if df.loc[i,str(day + 6)] == int(min((df.loc[i,'Column'],new_list[i-1]))):
                     pass
                 else:
-                    df.loc[i,str(day_2 + 6)] = a - sum(total_list)
-                    total_list.append(df.loc[i,str(day_2 + 6)])
+                    df.loc[i,str(day + 6)] = a - sum(total_list)
+                    total_list.append(df.loc[i,str(day + 6)])
     return total_list
 
 def main()
