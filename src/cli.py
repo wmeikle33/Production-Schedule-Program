51f3c1219074957c65748ee6e114d9822c819dce
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-27 00:46:25 +0800

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -14,8 +14,9 @@ app = typer.Typer(add_completion=False, help="Production Scheduling Program CLI"
 
 @app.command()
 def run(
-    value_list: str = typer.Option(..., "--value_list", help="Values for Production Program"),
+    value_list: list = typer.Option(..., "--value_list", help="Values for Production Program"),
     date: str = typer.Option(1, "--date", help="Date"),
+    new_values: list = typer.Option(..., "--value_list", help="Values for Production Program"),
     out_dir: Path = typer.Option(Path("data/"), "--out-dir", help="Output file path"),
 ):
     print("[bold green]Done![/bold green]")
